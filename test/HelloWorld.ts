e45e52f5c0e0bbd650d1ca06762eb0429c3946ea
Serhii Trachuk <s.trachuk@apiko.com>
2022-10-15 20:50:27 +0300

    Init project. Add Hello World contract

--- test/HelloWorld.ts
+++ test/HelloWorld.ts
@@ -1,10 +1,10 @@
-import "@nomiclabs/hardhat-ethers"
-import {ethers} from "hardhat"
-import {expect} from "chai"
-import {describe, it} from "node:test"
+import '@nomiclabs/hardhat-ethers'
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
 
-describe('hello word', function () {
-  it('should say hi', async function () {
+
+describe('hello word', function() {
+  it('should say hi', async function() {
     // 1. setup
     // 2. deploy contract
     // 3. call our function to test
@@ -13,6 +13,6 @@ describe('hello word', function () {
     const hello = await HelloWorld.deploy()
     await hello.deployed()
 
-    expect(await hello.hello()).to.equal('hello')
+    expect(await hello.hello()).to.equal('Hello world!')
   })
 })
